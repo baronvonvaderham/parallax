bc93a1f738f153c4ea841bd2d2f0b2918e2af97b
Scott Johnson <baronvonvaderham@gmail.com>
2023-03-29 17:16:30 -0400

    Change test db settings for in CircleCI

--- parallax/test_settings.py
+++ parallax/test_settings.py
@@ -1,1 +1,12 @@
 from parallax.settings import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME', 'parallax'),
+        'USER': os.getenv('DB_USER', 'parallax'),
+        'PASSWORD': os.getenv('DB_PASSWORD', 'parallax'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': 5432,
+    }
+}
